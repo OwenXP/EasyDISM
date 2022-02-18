c99552f329cf5fcd32e6e3a031b326a1c823db73
OwenXP <wesopower@gmail.com>
2022-02-18 19:50:15 +0900

    Update 1.2

--- main.c
+++ main.c
@@ -3,14 +3,14 @@
 
 int main() {
     int ch, save;
-    char a[100], b[100],c[100];
+    char a[100], b[100],c[100], d[100];
     int sex[1000];
     printf("EasyDISM\n");
     printf("Please Run To Administrator!!!\n");
 
     for (;;) {
         printf("Choose Options\n");
-        printf("1:Capture-Image, 2:Apply-Image, 3:ImageIndex, 4:Mount-Image, 5:Unmount-Image, 6:Exit\n");
+        printf("1:Capture-Image, 2:Apply-Image, 3:ImageIndex, 4:Mount-Image, 5:Unmount-Image, 6:Split-Image, 7:Exit\n");
         scanf("%d", &ch);
 
         if (ch == 1) {
@@ -29,14 +29,35 @@ int main() {
             printf("ImageFile\n");
             printf(">");
             scanf("%s", a);
-            printf("ApplyDir\n");
+            printf("Do you have SWM Files?(y,n)\n");
             printf(">");
-            scanf("%s", b);
-            printf("Index\n");
-            printf(">");
-            scanf("%s", c);
-            sprintf(sex ,"dism /Apply-Image /ImageFile:%s /ApplyDir:%s /Index:%s\n", a, b, c);
-            printf(system(sex) + "Exited...\n");
+            scanf("%d", &save);
+            if (save == 1) {
+                printf("SWMFile\n");
+                printf(">");
+                scanf("%s", d);
+                printf("ApplyDir\n");
+                printf(">");
+                scanf("%s", b);
+                printf("Index\n");
+                printf(">");
+                scanf("%s", c);
+                sprintf(sex ,"dism /Apply-Image /ImageFile:%s /swmfile:%s /ApplyDir:%s /Index:%s\n", a, d, b, c);
+                printf(system(sex) + "Exited...\n");
+            } else if (save == 2) {
+                printf("ApplyDir\n");
+                printf(">");
+                scanf("%s", b);
+                printf("Index\n");
+                printf(">");
+                scanf("%s", c);
+                sprintf(sex ,"dism /Apply-Image /ImageFile:%s /ApplyDir:%s /Index:%s\n", a, b, c);
+                printf(system(sex) + "Exited...\n");
+            } else {
+                printf("Error..\n");
+                break;
+            }
+
         } else if (ch == 3) {
             printf("ImageFile\n");
             printf(">");
@@ -74,6 +95,18 @@ int main() {
                 printf("Error.. Please Choose Yes or No\n");
             }
         } else if (ch == 6) {
+            printf("ImageFlie\n");
+            printf(">");
+            scanf("%s", a);
+            printf("SWMFile\n");
+            printf(">");
+            scanf("%s", b);
+            printf("FileSize\n");
+            printf(">");
+            scanf("%d", &save);
+            sprintf(sex, "Dism /Split-Image /ImageFile:%s /SWMFile:%s /FileSize:%d\n", a, b, save);
+            printf(system(sex) + "Exited...\n");
+        } else if (ch == 7) {
             break;
         } else {
             printf("Error, Please Choose Options..\n");
